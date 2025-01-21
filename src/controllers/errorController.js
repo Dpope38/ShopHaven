d969f5a29175b555daa47b3076ed97c2c2834e2a
popeopzy <popeopzy@gmail.com>
2025-01-21 23:12:26 +0100

    error handling fixed

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -40,30 +40,40 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-unused-vars
 import AppError from "../utils/appError.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 
-const handleCastErrorDb = (err) => {
-  const message = `Invalid ${err.value}: ${err.path}!`;
+// handle cast error
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
-const handleDuplicateFieldsDb = (err) => {
-  const msg = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
-  return new AppError(msg, 400);
+
+// Handling duplicate fields error
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new AppError(message, 400);
 };
 
-const validationErrorDb = (err) => {
+// Handling validation error
+const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
+
+// error handling for development
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
-    stack: err.stack,
     error: err,
+    stack: err.stack,
   });
 };
 
+// error handling for production
 const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
@@ -71,28 +81,32 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     });
   } else {
-    console.error(`Error: ${err}`);
+    console.error("Error", err);
     res.status(500).json({
       status: "error",
       message: "Something went very wrong",
     });
   }
 };
-
 const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    if (err.name === "CastError") err = handleCastErrorDb(err);
-    if (err.code === 11000) err = handleDuplicateFieldsDb(err);
-    if (err.name === "ValidationError") err = validationErrorDb(err);
-    sendErrorProd(res, err);
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError")
+      error = new AppError("Invalid token. Please log in again", 401);
+    if (error.name === "TokenExpiredError")
+      error = new AppError("Your token has expired! Please log in again", 401);
+
+    sendErrorProd(error, res);
   }
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
 };
+
 export default globalError;
